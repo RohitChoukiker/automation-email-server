4d512dfd7c4b15d5180408c35d444a407ec0d45e
RohitChoukiker <rohitchoukiker2803@gmail.com>
2025-11-30 14:05:10 +0530

    update

---
+++ node-server/test/sum.test.js
@@ -0,0 +1,9 @@
+const sum = require('./sum');
+
+// test('adds 5 + 7 to equal 12', () => {
+//   expect(sum(5, 7)).toBe(12);
+// });
+
+test('adds 5 + 7 to equal 12', () => {
+    expect(sum(5,7)).toBe(12);
+})
